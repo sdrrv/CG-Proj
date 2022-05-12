56423ee02ccd649bf8e940089da984f19739f12f
sdrrv <duartecelvas@gmail.com>
2022-05-12 17:40:01 +0100

    Init

---
+++ js/mesa.js
@@ -0,0 +1,193 @@
+/*global THREE, requestAnimationFrame, console*/
+
+var camera, scene, renderer;
+
+var geometry, material, mesh;
+
+var ball;
+
+var testi;
+
+function addTableLeg(obj, x, y, z) {
+    'use strict';
+
+    geometry = new THREE.CubeGeometry(2, 6, 2);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y - 3, z);
+    obj.add(mesh);
+}
+
+function addTableTop(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(60, 2, 20);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function createBall(x, y, z) {
+    'use strict';
+    
+    ball = new THREE.Object3D();
+    ball.userData = { jumping: true, step: 0 };
+    
+    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+    geometry = new THREE.SphereGeometry(4, 10, 10);
+    mesh = new THREE.Mesh(geometry, material);
+    
+    ball.add(mesh);
+    ball.position.set(x, y, z);
+    
+    scene.add(ball);
+}
+
+
+function createTable(x, y, z) {
+    'use strict';
+    
+    var table = new THREE.Object3D();
+    
+    material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+   
+    addTableTop(table, 0, 0, 0);
+    addTableLeg(table, -25, -1, -8);
+    addTableLeg(table, -25, -1, 8);
+    addTableLeg(table, 25, -1, 8);
+    addTableLeg(table, 25, -1, -8);
+    
+    scene.add(table);
+    
+    table.position.x = x;
+    table.position.y = y;
+    table.position.z = z;
+}
+
+
+function createTesti(){
+    'use strict';
+
+    const geometry = new THREE.TorusGeometry(1.24,0.20,8,360,360);
+
+    const material = new THREE.MeshBasicMaterial({color: 0xffffff});
+
+    testi = new THREE.Mesh(geometry,material);
+    testi.rotation.x = Math.PI / 2;
+    testi.rotation.y = 0;
+    testi.rotation.z = 0;
+
+
+    scene.add(testi);
+}
+
+function createScene() {
+    'use strict';
+    
+    scene = new THREE.Scene();
+    
+
+    scene.add(new THREE.AxisHelper(10));
+
+    //createTable(0, 8, 0);
+    //createBall(0, 0, 15);
+    createTesti();
+}
+
+function createCamera() {
+    'use strict';
+    camera = new THREE.PerspectiveCamera(70,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    camera.position.x = 5;
+    camera.position.y = 5;
+    camera.position.z = 5;
+    camera.lookAt(scene.position);
+}
+
+function onResize() {
+    'use strict';
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+
+}
+
+function onKeyDown(e) {
+    'use strict';
+    
+    switch (e.keyCode) {
+    case 65: //A
+    case 97: //a
+        scene.traverse(function (node) {
+            if (node instanceof THREE.Mesh) {
+                node.material.wireframe = !node.material.wireframe;
+            }
+        });
+        break;
+    case 83:  //S
+    case 115: //s
+        ball.userData.jumping = !ball.userData.jumping;
+        break;
+    case 69:  //E
+    case 101: //e
+        scene.traverse(function (node) {
+            if (node instanceof THREE.AxisHelper) {
+                node.visible = !node.visible;
+            }
+        });
+        break;
+    }
+}
+
+function render() {
+    'use strict';
+    renderer.render(scene, camera);
+}
+
+
+function createLight(){
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    scene.add( directionalLight );
+}
+
+function init() {
+    'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+   
+    createScene();
+    createCamera();
+    createLight();
+    
+    render();
+    
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("resize", onResize);
+}
+
+
+var loler = 0;
+function animate() {
+
+    'use strict';
+    loler += 0.005;
+    let lol = 3 * (Math.sin(loler));
+    testi.scale.set(lol,lol,lol);
+    testi.position.y = 3 * Math.cos(loler)
+    /*if (ball.userData.jumping) {
+        ball.userData.step += 0.04;
+        ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
+        ball.position.z = 15 * (Math.cos(ball.userData.step));
+    }*/
+    render();
+    
+    requestAnimationFrame(animate);
+}
+
