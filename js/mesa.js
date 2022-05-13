d3603218a4a4fb610d6434d11c7c375a62665968
sdrrv <duartecelvas@gmail.com>
2022-05-13 16:07:52 +0100

    Prettier

--- js/mesa.js
+++ js/mesa.js
@@ -2,116 +2,78 @@
 
 var camera, scene, renderer;
 
-var geometry, material, mesh;
+//-------------------------FIRST------------------------------------
+var firstRing1, firstRing2, firstBall;
 
-var ball;
-
-var testi, testi2, leBall1;
-
-
-function addTableLeg(obj, x, y, z) {
-    'use strict';
-
-    geometry = new THREE.CubeGeometry(2, 6, 2);
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y - 3, z);
-    obj.add(mesh);
-}
-
-function addTableTop(obj, x, y, z) {
-    'use strict';
-    geometry = new THREE.CubeGeometry(60, 2, 20);
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
-    obj.add(mesh);
-}
-
-function createBall(x, y, z) {
-    'use strict';
-    
-    ball = new THREE.Object3D();
-    ball.userData = { jumping: true, step: 0 };
-    
-    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-    geometry = new THREE.SphereGeometry(4, 10, 10);
-    mesh = new THREE.Mesh(geometry, material);
-    
-    ball.add(mesh);
-    ball.position.set(x, y, z);
-    
-    scene.add(ball);
-}
-
-
-function createTable(x, y, z) {
-    'use strict';
-    
-    var table = new THREE.Object3D();
-    
-    material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
-   
-    addTableTop(table, 0, 0, 0);
-    addTableLeg(table, -25, -1, -8);
-    addTableLeg(table, -25, -1, 8);
-    addTableLeg(table, 25, -1, 8);
-    addTableLeg(table, 25, -1, -8);
-    
-    scene.add(table);
-    
-    table.position.x = x;
-    table.position.y = y;
-    table.position.z = z;
-}
-
-
-function createTesti(){
+function firstCreateRing1(){
     'use strict';
 
     const geometry = new THREE.TorusGeometry(1.24,0.20,8,360,360);
 
     const material = new THREE.MeshToonMaterial({color: 0x1ca8ff});
 
-    testi = new THREE.Mesh(geometry,material);
-    testi.rotation.x = Math.PI / 2;
-    testi.rotation.y = 0;
-    testi.rotation.z = 0;
+    firstRing1 = new THREE.Mesh(geometry,material);
+    firstRing1.rotation.x = Math.PI / 2;
+    firstRing1.rotation.y = 0;
+    firstRing1.rotation.z = 0;
 
 
-    scene.add(testi);
+    scene.add(firstRing1);
 }
 
-
-function createBall1(x, y ,z){
+function firstCreateBall(x, y ,z){
     'use strict';
 
     const geometry = new THREE.SphereGeometry(1,100,100,0,360,0,180);
     const material = new THREE.MeshNormalMaterial({color: 0x5bba1a});
-    leBall1 = new THREE.Mesh(geometry,material);
+    firstBall = new THREE.Mesh(geometry,material);
 
-    leBall1.position.x = x;
-    leBall1.position.y = y;
-    leBall1.position.z = z;
+    firstBall.position.x = x;
+    firstBall.position.y = y;
+    firstBall.position.z = z;
 
 
-    scene.add(leBall1);
+    scene.add(firstBall);
 
 }
 
-function createTesti2(){
+function firstCreateRing2(){
     'use strict';
 
     const geometry = new THREE.TorusGeometry(1.24,0.20,8,360,360);
 
     const material = new THREE.MeshToonMaterial({color: 0xde28db});
 
-    testi2 = new THREE.Mesh(geometry,material);
-    testi2.rotation.x = Math.PI / 2;
-    testi2.rotation.y = 0;
-    testi2.rotation.z = 0;
+    firstRing2 = new THREE.Mesh(geometry,material);
+    firstRing2.rotation.x = Math.PI / 2;
+    firstRing2.rotation.y = 0;
+    firstRing2.rotation.z = 0;
 
 
-    scene.add(testi2);
+    scene.add(firstRing2);
 }
+
+let firstBallSpeed = 0;
+let firstRingSpeed = 0;
+
+function animateFirst(){
+    'use strict';
+
+    firstRingSpeed += 0.002;
+    firstBallSpeed += 0.01;
+
+    let scaleFactor = 3 * (Math.cos(firstRingSpeed));
+    firstRing1.scale.set(scaleFactor,scaleFactor,scaleFactor);
+    firstRing1.position.y = Math.abs(3 * Math.sin(firstRingSpeed));
+
+    let scaleFactor2 = 5 * (Math.sin(firstRingSpeed));
+    firstRing2.scale.set(scaleFactor2,scaleFactor2,scaleFactor2);
+    firstRing2.position.y = Math.abs(5 * Math.cos(firstRingSpeed));
+    firstBall.position.y = 4 * Math.sin(firstBallSpeed);
+
+}
+
+//-------------------------------------------------------------------------
 
 function createScene() {
     'use strict';
@@ -121,11 +83,10 @@ function createScene() {
 
     scene.add(new THREE.AxisHelper(10));
 
-    //createTable(0, 8, 0);
-    //createBall(0, 0, 15);
-    createTesti();
-    createTesti2();
-    createBall1(0,0,0);
+
+    firstCreateRing1();
+    firstCreateRing2();
+    firstCreateBall(0,0,0);
 
 }
 
@@ -209,35 +170,11 @@ function init() {
     window.addEventListener("resize", onResize);
 }
 
-
-var loler = 0;
-var heyo = 0;
-var leBallSpeed = 0;
 function animate() {
-
     'use strict';
-    loler += 0.007;
-    heyo += 0.002;
-    leBallSpeed += 0.05;
-    let lol = 3 * (Math.cos(loler));
-    testi.scale.set(lol,lol,lol);
-    testi.position.y = Math.abs(3 * Math.sin(loler));
-    let lol1 = 5 * (Math.sin(loler));
-    testi2.scale.set(lol1,lol1,lol1);
-    testi2.position.y = Math.abs(5 * Math.cos(loler));
 
-    //---------------------------------------------------------------------
-    leBall1.position.y = 4 * Math.sin(leBallSpeed);
-    //---------------------------------------------------------------------
+    animateFirst(); // First
 
-
-
-
-    /*if (ball.userData.jumping) {
-        ball.userData.step += 0.04;
-        ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
-        ball.position.z = 15 * (Math.cos(ball.userData.step));
-    }*/
     render();
     
     requestAnimationFrame(animate);
